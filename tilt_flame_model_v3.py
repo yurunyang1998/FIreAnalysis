3329988d3daafdf24538ad7cf89d83f8950438fd
jiayangvivid <676336848@qq.com>
2020-10-26 00:16:54 +0800

    主要修改theta位置、exponential_number和b点约束
    theta=theta/180*pi 应该在H = fireHeight / (cos(theta))，否则计算得到的H不正确。
    tilt_flame_rad_heat_pb()等函数中的exponential_number过大，拟合曲线效果太差，在这个版本中改为5.
    line394中X_a应该<=0，if语句需要修改。

--- tilt_flame_model_v3.py
+++ tilt_flame_model_v3.py
@@ -45,12 +45,12 @@ def close_handle(evt):
 #     b=X/R
 
 def FV1_func(fireHeight, fireWidth, theta, R_distance ):
+    theta=theta/180*pi
     H = fireHeight / (cos(theta))
     a = H / fireWidth
     b = R_distance / fireWidth
     if b==1 or b==-1:
         b=b+0.0001
-    theta=theta/180*pi
     fv_val=-a*cos(theta)/(pi*(b-a*sin(theta)))*atan(sqrt((b-1)/(b+1)))+a*cos(theta)/(pi*(b-a*sin(theta)))\
     *(a**2+(b+1)**2-2*b*(1+a*sin(theta)))/(sqrt(a**2+(b+1)**2-2*a*(b+1)*sin(theta))*sqrt(a**2+(b-1)**2-2*a*(b-1)*sin(theta)))\
     *atan(sqrt((a**2+(b+1)**2-2*a*(b+1)*sin(theta))/(a**2+(b-1)**2-2*a*(b-1)*sin(theta)))*sqrt((b-1)/(b+1)))+cos(theta)/(pi*sqrt(1+(b**2-1)*cos(theta)**2))\
@@ -60,13 +60,12 @@ def FV1_func(fireHeight, fireWidth, theta, R_distance ):
 #6.1.2水平视角系数
 #6.1.2水平视角系数计算函数
 def FH1_func(fireHeight, fireWidth, theta, R_distance):
-
+    theta=theta/180*pi
     H = fireHeight / (cos(theta))
     a=H/fireWidth
     b=R_distance/fireWidth
     if b==1 or b==-1:
         b=b+0.0001
-    theta=theta/180*pi
     x1=a**2+(b+1)**2-2*a*(b+1)*sin(theta)
     x2=a**2+(b-1)*(b-1)-2*a*(b-1)*sin(theta)
     fh_val=atan(pow(((b+1)/(b-1)),0.5))/pi+sin(theta)/(pi*pow(1+(b**2-1)*cos(theta)*cos(theta),0.5))\
@@ -78,6 +77,7 @@ def FH1_func(fireHeight, fireWidth, theta, R_distance):
 #6.2当观察者位于垂直于火焰倾斜方向的位置时，其视角系数
 #6.2.1水平视角系数：
 def FH2_func(fireHeight, fireWidth, theta, R_distance):
+    theta=theta/180*pi
     H = fireHeight / (cos(theta))
     a = H / fireWidth
     b = R_distance / fireWidth
@@ -91,6 +91,7 @@ def FH2_func(fireHeight, fireWidth, theta, R_distance):
 
 #6.2.2垂直视角系数：
 def FV2_func(fireHeight, fireWidth, theta, R_distance):
+    theta=theta/180*pi
     H = fireHeight / (cos(theta))
     a = H / fireWidth
     b = R_distance / fireWidth
@@ -268,7 +269,7 @@ def tilt_flame_rad_heat_pa(H, W, theta, epsilon, T, R_distance, rad_heat):
         x_xa=np.array(y)
         x_xa=x_xa.astype(np.float64)
         y_xa=x
-        exponential_number=8
+        exponential_number=5
         f2 = np.polyfit(x_xa, y_xa, exponential_number)
         X_a=np.polyval(f2, rad_heat)
         #print(X_a_array)
@@ -289,7 +290,7 @@ def tilt_flame_hazardous_radius_xa(H, R, theta, epsilon, T, fig):
     # x_xa=np.array(y)
     # x_xa=x_xa.astype(np.float64)
     # y_xa=x
-    # exponential_number=8
+    # exponential_number=5
     # f2 = np.polyfit(x_xa, y_xa, exponential_number)
     # X_a_array=np.polyval(f2, rad_heat)
     # #print(X_a_array)
@@ -322,7 +323,7 @@ def tilt_flame_hazardous_radius_xb(H, W, theta, epsilon, T, R_distance, fig, rad
     # x_xa=np.array(y)
     # x_xa=x_xa.astype(np.float64)
     # y_xa=x
-    # exponential_number=8
+    # exponential_number=5
     # f2 = np.polyfit(x_xa, y_xa, exponential_number)
     # X_a_array=np.polyval(f2, rad_heat)
     # #print(X_a_array)
@@ -355,7 +356,7 @@ def tilt_flame_hazardous_radius_xc(H, W, theta, epsilon, T, R_distance, fig, rad
     # x_xa=np.array(y)
     # x_xa=x_xa.astype(np.float64)
     # y_xa=x
-    # exponential_number=8
+    # exponential_number=5
     # f2 = np.polyfit(x_xa, y_xa, exponential_number)
     # X_a_array=np.polyval(f2, rad_heat)
     # #print(X_a_array)
@@ -380,22 +381,22 @@ def tilt_flame_hazardous_radius_xc(H, W, theta, epsilon, T, R_distance, fig, rad
 
 # 给定辐射热流rad_heat时，b点位置的计算函数（x轴负半轴位置）
 def tilt_flame_rad_heat_pb(H, W, theta, epsilon, T, R_distance , rad_heat):
-    return tilt_flame_rad_heat_pa(H, W, 180-theta, epsilon, T, R_distance, rad_heat)
-    # try:
-    #     x,y = draw_rad_heat_flux_curve_FH1_x_neg_withoutDraw(H, W, theta, epsilon, T, R_distance)
-    #     x_xa=np.array(y)
-    #     x_xa=x_xa.astype(np.float64)
-    #     y_xa=x
-    #     exponential_number=8
-    #     f2 = np.polyfit(x_xa, y_xa, exponential_number)
-    #     X_a=np.polyval(f2, rad_heat)
-    #     #print(X_a_array)
-    #     if (X_a < 0):
-    #         X_a = 0
-    #     return X_a
-    # except Exception as e:
-    #     traceback.print_exc()
-    #     return 0
+    #return tilt_flame_rad_heat_pa(H, W, 180-theta, epsilon, T, R_distance, rad_heat)
+    try:
+        x,y = draw_rad_heat_flux_curve_FH1_x_neg_withoutDraw(H, W, theta, epsilon, T, R_distance)
+        x_xa=np.array(y)
+        x_xa=x_xa.astype(np.float64)
+        y_xa=x
+        exponential_number=5
+        f2 = np.polyfit(x_xa, y_xa, exponential_number)
+        X_a=np.polyval(f2, rad_heat)
+        #print(X_a_array)
+        if (X_a > 0):
+            X_a = 0
+        return X_a
+    except Exception as e:
+        traceback.print_exc()
+        return 0
 # 给定辐射热流rad_heat时，c点位置的计算函数（y轴负半轴位置）
 def tilt_flame_rad_heat_pc(H, W, theta, epsilon, T, R_distance, rad_heat):
     try:
@@ -403,7 +404,7 @@ def tilt_flame_rad_heat_pc(H, W, theta, epsilon, T, R_distance, rad_heat):
         x_xa=np.array(y)
         x_xa=x_xa.astype(np.float64)
         y_xa=x
-        exponential_number=8
+        exponential_number=5
         f2 = np.polyfit(x_xa, y_xa, exponential_number)
         X_a=np.polyval(f2, rad_heat)
         #print(X_a_array)
@@ -429,7 +430,7 @@ def plot_abc(H, W, theta, epsilon, T, R_distance, rad_heat ,fig):
         # fig = plt.figure()
         ax = fig.add_subplot(111)
         R_3=[X_a, X_b, Y_c]
-        colors = ["blue","yellow","green"]
+        colors = ["blue","orange","green"]
         for i in range(3):
             cir = Circle(xy = (0.0, 0.0), radius=R_3[i], alpha=0.5, linewidth=2, fill=False, color= colors[i])
             ax.add_patch(cir)
@@ -441,20 +442,20 @@ def plot_abc(H, W, theta, epsilon, T, R_distance, rad_heat ,fig):
             plt.axis('equal')   #changes limits of x or y axis so that equal increments of x and y have the same length
 
         ax.plot(X_a, 0, 'ro')
-        ax.plot((-1)*X_b, 0, 'ro')
+        ax.plot(X_b, 0, 'ro')
         ax.plot(0, (-1)*Y_c, 'ro')
 
         plt.text(0, 0, 'flame', ha='right', wrap=True)
         plt.text(int(X_a), 0, 'a', ha='right', wrap=True)
-        plt.text(int(X_b)*(-1), 0, 'b', ha='left', wrap=True)
+        plt.text(int(X_b), 0, 'b', ha='left', wrap=True)
         plt.text(0, int(Y_c)*(-1), 'c', ha='left', wrap=True)
         # plt.legend()
 
-        plt.pause(3)
+        plt.pause(0.4)
         plt.show()
     except Exception as e:
         traceback.print_exc()
-        plt.pause(3)
+        plt.pause(0.4)
 
 
 
@@ -475,15 +476,21 @@ def plot_abc(H, W, theta, epsilon, T, R_distance, rad_heat ,fig):
 
 #当热流密度为4kW/m2时，找出对应4 kW/m2时a点、b点、c点的位置，以这些位置为半径，分别化同心圆
 #名称：以火焰中心为中心的伤害范围示意图
-# plot_abc(18, 13, 75, 4)
+fig = plt.figure()
+plot_abc(1, 0.8, 75, 0.5, 600, 10, 200, fig)
 
+#plot_abc(H, W, theta, epsilon, T, R_distance, rad_heat ,fig)
 #假设人在a点的伤害半径
 #名称：假设人在a点不同辐射热流值的伤害范围
-# tilt_flame_hazardous_radius_xa(0.17, 0.85, 70)
-# #名称：假设人在b点不同辐射热流值的伤害范围
-# tilt_flame_hazardous_radius_xb(0.17, 0.85, 70)
-# #名称：假设人在c点不同辐射热流值的伤害范围
-# tilt_flame_hazardous_radius_xc(0.17, 0.85, 70)
+#tilt_flame_hazardous_radius_xa(0.17, 0.85, 70)
+##名称：假设人在b点不同辐射热流值的伤害范围
+#tilt_flame_hazardous_radius_xb(0.17, 0.85, 70)
+##名称：假设人在c点不同辐射热流值的伤害范围
+#tilt_flame_hazardous_radius_xc(0.17, 0.85, 70)
+
+
+
+
 
 
 
